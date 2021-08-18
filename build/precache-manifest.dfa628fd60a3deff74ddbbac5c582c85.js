6369d434af977b993e72bb67f90a185290b6ac4a
Hammad <mrhammad.009@gmail.com>
2021-08-19 00:04:13 +0500

    'ok'

---
+++ build/precache-manifest.dfa628fd60a3deff74ddbbac5c582c85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d7c46ec697aff483293194c0d6b700",
+    "url": "/index.html"
+  },
+  {
+    "revision": "691f7ee83c37e15c9151",
+    "url": "/static/css/main.fcc82020.chunk.css"
+  },
+  {
+    "revision": "ca77627b6a30990c1a89",
+    "url": "/static/js/2.3c152af1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3c152af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "691f7ee83c37e15c9151",
+    "url": "/static/js/main.306fc87f.chunk.js"
+  },
+  {
+    "revision": "f13d5fb20258b992088c",
+    "url": "/static/js/runtime-main.a6b2f021.js"
+  }
+]);
